a5537f085ea9d477ca578677d4d42132006cd216
Anudeep <anudeep.hanuman@gmail.com>
2022-08-18 16:17:03 +0530

    added community section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from './components/Navbar';
 import Hero from "./components/Hero";
 import Faq from "./components/Faq";
 import Footer from "./components/Footer";
+import Community from './components/Community';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Hero/>
+      <Community/>
       <Faq/>
       <Footer/>
     </div>
---
+++ src/components/Community.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import people from "../assets/images/people.png";
+import "./css/Community.css";
+
+export default function Community() {
+    return (
+        <section className="community">
+            <h1 className="section-heading">Our Community</h1>
+            <div className="community-content">
+                <div className="community-details">
+                    <p className='community-para'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur obcaecati sequi accusamus facilis asperiores autem iusto quaerat ducimus aspernatur natus vel, dolorum rerum laboriosam cumque placeat dignissimos ut cum doloremque? Provident hic dolorem temporibus eum quibusdam autem quisquam quidem sunt aspernatur, incidunt repellendus, accusantium adipisci nobis? Ipsa debitis ipsum earum?</p>
+                </div>
+                <div className="community-image">
+                    <img src={people} alt="community" srcset="" />
+                </div>
+            </div>
+            <a href="http://" className='community-button'>Join Our Community</a>
+        </section>
+    )
+}
