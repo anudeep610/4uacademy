70e089cadc46b369cd7d0f86431b3ed5b5b6506c
Aravapalli Naga Venkata Sai Anudeep <66114152+anudeep610@users.noreply.github.com>
2022-09-30 11:50:45 +0530

    Update Courses.js

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -56,7 +56,7 @@ export default function Courses() {
                         </div>
                         <div className="course-outcomes">
                             <h6 className="course-outcomes-heading">Batch Starts On</h6>
-                            <p className="course-outcomes-content"> 30th September </p>
+                            <p className="course-outcomes-content"> 5th October </p>
                         </div>
                         <div className="course-outcomes">
                             <h6 className="course-outcomes-heading">What you'll learn?</h6>
